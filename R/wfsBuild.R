86969004bb97f10d543208078673ef376ff9c33c
Willem <willem.stolte@deltares.nl>
2020-09-22 13:11:42 +0200

    made suitable for new Geoserver. The new geoserver is available from within Deltares, and not from outside.

--- R/wfsBuild.R
+++ R/wfsBuild.R
@@ -1,51 +1,51 @@
 
 #' Create url to be used as wfs query
-#' 
-#' This function is used internally at Deltares to query the North Sea and Wadden sea 
-#' validation database. The url can then be used to download validation data. 
+#'
+#' This function is used internally at Deltares to query the North Sea and Wadden sea
+#' validation database. The url can then be used to download validation data.
 #' @param cql_list list with cql conditions
 #' @param outputformat desired format for download
 #' @param maxFeatures maximum number of features to download
 #' @columns character vector of columns to download
-#' @example 
+#' @example
 #' cql_list = list(p35preflabel = "Water body nitrate",
 #' location_code = "DOOVBWT"
 #' )
 #' columns = c("location_code", "date", "depth", "parameter_label", "value", "unit_preflabel", "quality_code")
-#' 
+#'
 #' url = create_url(cql_list = cql_list)
 #' read_csv(url)
-#' 
+#'
 create_url <- function(cql_list, outputFormat = "csv", maxFeatures = NULL, columns = NULL) {
-  
+
   require(httr)
-  
+
   if(is.null(columns)) propertyName = NULL else propertyName = paste("NWDM", columns, sep = ":", collapse = ",")
-  
+
   if(is.null(cql_list)) cql_filter = NULL else cql_filter = paste0("(",paste(names(cql_list),paste0("'", cql_list, "'"),sep="=",collapse=" and " ),")")
-  
+
   request <- structure(
-    list(scheme = "https", 
-         hostname = "marineprojects.openearth.nl", 
-         port = NULL, 
-         path = "geoserver/NWDM/ows", 
+    list(scheme = "https",
+         hostname = "http://al-276.xtr.deltares.nl:8080",
+         port = NULL,
+         path = "geoserver/NWDM/ows",
          query = list(
-           service = "WFS", 
-           version = "1.0.0", 
-           request = "GetFeature", 
+           service = "WFS",
+           version = "1.0.0",
+           request = "GetFeature",
            typeName = "NWDM:measurement_view",
            cql_filter = cql_filter,
-           outputFormat = outputFormat, 
+           outputFormat = outputFormat,
            maxFeatures = maxFeatures,
            propertyName = propertyName
-         ), 
-         params = NULL, 
-         fragment = NULL, 
-         username = NULL, 
+         ),
+         params = NULL,
+         fragment = NULL,
+         username = NULL,
          password = NULL
-    ), 
+    ),
     class = "url")
-  
+
   url <- httr::build_url(request)
 }
 
